361955eb04f99829075478b77412babda64fe477
Yunping <qufang83@gmail.com>
2014-08-20 18:48:11 +0800

    Update solution. Optimize space

--- ValidSudoku.h
+++ ValidSudoku.h
@@ -15,52 +15,41 @@ Solution: o(n^2), one pass traverse.
 1. Use a row map, a col map, and 3 cell maps, to record the number that has been used.
 2. Traverse by rows/cols, check each elem in every row/col/cell. Reset the map for 
    rows/cols every row/col. Reset the cell maps every 3 rows.
+3. Use bit manipulation. Space: sizeof(short)*2 + sizeof(int)
 */
    
 class Solution {
 public:
-    // My own solution
     bool isValidSudoku(vector<vector<char> > &board) {
-        // first check rows
-        bool row_used[9];
-        bool col_used[9];
-        bool cell_used[3][9];
-        
-        for (int i = 0; i < 9; ++i) {
-            memset(row_used, false, 9);
-            memset(col_used, false, 9);
-            if (i % 3 == 0) {
-                memset(cell_used[0], false, 9);
-                memset(cell_used[1], false, 9);
-                memset(cell_used[2], false, 9);
-            }
-            for (int j = 0; j < 9; ++j) {
-                // check rows
+        const int N = 9;
+        short row_used = 0, col_used = 0;
+        int cell_used = 0;
+        for (int i = 0; i < N; ++i) {
+            row_used = col_used = 0;
+            if (i % 3 == 0)
+                cell_used = 0;
+            
+            for (int j = 0; j < N; ++j) {
                 char row_elem = board[i][j];
-                if (row_elem >= '1' && row_elem <= '9') {
-                    if (row_used[row_elem - '1'])
+                if (row_elem != '.') {
+                    // check rows
+                    if (row_used & (1<<(row_elem - '1')))
                         return false;
-                    row_used[row_elem - '1'] = true;
-                } else if (row_elem != '.')
-                    return false;
+                    row_used |= (1<<(row_elem - '1'));
                     
+                    // check cells
+                    if (cell_used & (1<<(N*(j/3) + row_elem - '1')))
+                        return false;
+                    cell_used |= (1<<(N*(j/3) + row_elem - '1'));
+                }
+                
                 // check cols
                 char col_elem = board[j][i];
-                if (col_elem >= '1' && col_elem <= '9') {
-                    if (col_used[col_elem - '1'])
+                if (col_elem != '.') {
+                    if (col_used & (1<<(col_elem - '1')))
                         return false;
-                    col_used[col_elem - '1'] = true;
-                } else if (col_elem != '.')
-                    return false;
-                    
-                // check cells
-                char cell_elem = board[i][j];
-                if (cell_elem >= '1' && cell_elem <= '9') {
-                    if (cell_used[j/3][cell_elem - '1'])
-                        return false;
-                    cell_used[j/3][cell_elem - '1'] = true;
-                } else if (cell_elem != '.')
-                    return false;
+                    col_used |= (1<<(col_elem - '1'));
+                }
             }
         }
         
