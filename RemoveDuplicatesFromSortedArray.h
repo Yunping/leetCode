3f1dee51d5dbe60632f7052b91a146699e8bb6f9
Yunping <qufang83@gmail.com>
2015-11-21 17:31:26 +0800

    [Updated] RemoveDuplicatesFromSortedArray.

--- RemoveDuplicatesFromSortedArray.h
+++ RemoveDuplicatesFromSortedArray.h
@@ -1,34 +1,34 @@
 /*
 Problem: Remove Duplicates from Sorted Array
-
 Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
 Do not allocate extra space for another array, you must do this in place with constant memory.
 
 For example,
-Given input array A = [1,1,2],
+Given input array nums = [1,1,2],
 
-Your function should return length = 2, and A is now [1,2].
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 
-Source: https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/
+==========================================================================================
 
 Author: Yunping, qufang83@gmail.com
 Date: Aug 1, 2014
+Update: 11/21/2015
 Difficulty: *
-Review: ***
-Solution: No need to use too many vars.
-
+Review: *^
+Solution: two-pointers
 */
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (i > 0 && A[i] == A[i-1])
-                continue;
-            A[j++] = A[i];
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int i = 0, j = i + 1;
+        int len = nums.size();
+        while (j < len) {
+            if (nums[i] != nums[j])
+                nums[++i] = nums[j];
+            ++j;
         }
-        
-        return j;
+        return (i+1);
     }
 };
