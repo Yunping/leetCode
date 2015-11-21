e5d17c4ead7ec11313b153714e98a3b435f97017
Yunping <qufang83@gmail.com>
2015-11-21 17:29:23 +0800

    [Updated]Remove Element: Added two solutions.

--- RemoveElement.h
+++ RemoveElement.h
@@ -1,29 +1,74 @@
 /*
 Problem: Remove Element
+
 Given an array and a value, remove all instances of that value in place and return the new length.
 
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
-Source: https://oj.leetcode.com/problems/remove-element/
+==========================================================================================
 
 Author: Yunping, qufang83@gmail.com
 Date: Aug 1, 2014
+Update: 11/212015
 Difficulty: *^
-Review: ***
-Solution: Updated. Use two indexes.
+Review: *^
+Solution:
+1. two pointers
+2. partition
+3. two pointers and swap to-delete elements to the end
 
 */
-
 class Solution {
 public:
-    int removeElement(int A[], int n, int elem) {
-        if (!A || n <= 0) return 0;
-        int i = 0;
-        for (int j = 0; j < n; ++j) {
-            if (A[j] != elem)
-                A[i++] = A[j];
+    // solution 1: two pointers
+    int removeElement1(vector<int>& nums, int val) {
+        if (nums.empty()) return 0;
+        int i = 0, j = 0;
+        int len = nums.size();
+        while (j < len) {
+            if (nums[j] != val)
+                nums[i++] = nums[j];
+            ++j;
         }
         
         return i;
+    }
+    
+    // solution 2: partition
+    int removeElement2(vector<int>& nums, int val) {
+        if (nums.empty()) return 0;
+        int i = -1, j = 0;
+        int len = nums.size();
+        while (j < len) {
+            if (nums[j] == val)
+                ++j;
+            else
+                swap(nums[++i], nums[j++]);
+        }
+        return i+1;
+    }
+    
+    // solution 3:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.empty()) return 0;
+        int len = nums.size();
+        int i = 0, j = len - 1;
+        while (i <= j) {
+            while (i <= j && nums[i] != val)
+                ++i;
+            while (i <= j && nums[j] == val)
+                --j;
+            if (i < j)
+                swap(nums[i++], nums[j--]);
+        }
+        
+        return (j + 1);
+    }
+    
+private:
+    void swap(int &a, int &b) {
+        int t = a;
+        a = b;
+        b = t;
     }
 };
